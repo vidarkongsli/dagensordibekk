8164e9c2ab237cf95c3e22504b40fb7df735f6bf
Vidar Kongsli <vidarkongsli@yahoo.com>
2010-03-12 09:13:02 +0100

    Fixed code style. Changed sorting of words

--- application/controllers/word.py
+++ application/controllers/word.py
@@ -1,112 +1,112 @@
-import os
-import cgi
-import urllib
-import logging
 from application.authorization import Authorization
+from application.controllers.core import CoreHandler
+from application.model import Ord, Bidragsyter
 from google.appengine.api import users
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp.util import login_required
-from application.controllers.core import CoreHandler
-from application.model import Ord, Bidragsyter
+import cgi
+import logging
+import os
+import urllib
 
 class NyttOrdHandler(CoreHandler):
-	@login_required
-	def get(self):
-		Authorization.authorize(self)
-		self.renderUsingTemplate('../../views/forslagsskjema.html', {})
+    @login_required
+    def get(self):
+        Authorization.authorize(self)
+        self.renderUsingTemplate('../../views/forslagsskjema.html', {})
 
 class ForslagHandler(CoreHandler):
-	def post(self):
-		Authorization.authorize(self)
-		ord = cgi.escape(self.request.get('ord')).lower()
-		besk = cgi.escape(self.request.get('beskrivelse'))
-		
-		exists = db.GqlQuery("SELECT * FROM Ord WHERE navn = :1", ord)
-		errorCode = 0
-		if exists.count() > 0:
-			errorCode = 1
-		else:
-			bidrager = Bidragsyter.hent(users.get_current_user())
-			Ord(navn=ord, beskrivelse=besk, bidragsyter=bidrager.key()).put()
-		
-		self.renderAsJson({ 'errorCode':errorCode })
-	
+    def post(self):
+        Authorization.authorize(self)
+        ord = cgi.escape(self.request.get('ord')).lower()
+        besk = cgi.escape(self.request.get('beskrivelse'))
+        
+        exists = db.GqlQuery("SELECT * FROM Ord WHERE navn = :1", ord)
+        errorCode = 0
+        if exists.count() > 0:
+            errorCode = 1
+        else:
+            bidrager = Bidragsyter.hent(users.get_current_user())
+            Ord(navn=ord, beskrivelse=besk, bidragsyter=bidrager.key()).put()
+        
+        self.renderAsJson({ 'errorCode':errorCode })
+    
 class TilGodkjenningHandlerGammel(CoreHandler):
-	@login_required
-	def get(self):
-		alle_ord = Ord.all().filter("arbeidsflytstilstand =", 0).order('navn')
-		self.renderUsingTemplate('../../views/paavalg_gammel.html', { 'ord':alle_ord })
+    @login_required
+    def get(self):
+        alle_ord = Ord.all().filter("arbeidsflytstilstand =", 0).order('navn')
+        self.renderUsingTemplate('../../views/paavalg_gammel.html', { 'ord':alle_ord })
 
 class TilGodkjenningHandler(CoreHandler):
-	@login_required
-	def get(self):
-		if 'application/json' in self.request.headers['Accept']:
-			off = int(self.request.get('offset'))
-			alle_ord = Ord.all().filter("arbeidsflytstilstand =", 0).order('navn').fetch(20, offset=off)
-			self.renderAsJson({ 'offset': len(alle_ord) + off, 'ord': map(lambda ord: {
-				'navn' : ord.navn,
-				'id' : ord.key().id(),
-				'beskrivelse' : ord.beskrivelse,
-				'for' : len(ord.stemmerFor),
-				'mot' : len(ord.stemmerMot),
-				'bidrager' : {
-					'navn' : ord.bidragsyter.visningsnavn() if ord.bidragsyter else ord.bnavn(),
-					'id' : str(ord.bidragsyter.key()) if ord.bidragsyter else None}}, alle_ord) })
-		else:
-			self.renderUsingTemplate('../../views/paavalg.html', { })
-			
+    @login_required
+    def get(self):
+        if 'application/json' in self.request.headers['Accept']:
+            off = int(self.request.get('offset'))
+            alle_ord = Ord.all().filter("arbeidsflytstilstand =", 0).order('forslagstidspunkt').fetch(20, offset=off)
+            self.renderAsJson({ 'offset': len(alle_ord) + off, 'ord': map(lambda ord: {
+                'navn' : ord.navn,
+                'id' : ord.key().id(),
+                'beskrivelse' : ord.beskrivelse,
+                'for' : len(ord.stemmerFor),
+                'mot' : len(ord.stemmerMot),
+                'bidrager' : {
+                    'navn' : ord.bidragsyter.visningsnavn() if ord.bidragsyter else ord.bnavn(),
+                    'id' : str(ord.bidragsyter.key()) if ord.bidragsyter else None}}, alle_ord) })
+        else:
+            self.renderUsingTemplate('../../views/paavalg.html', { })
+            
 class NesteDagensOrdHandler(CoreHandler):
-	@login_required
-	def get(self):
-		alle_ord = []
-		alle_nye_godkjente_ord = Ord.all().filter("arbeidsflytstilstand =", 1).filter("harVaertDagensOrd =", False).order('forslagstidspunkt')
-		if not alle_nye_godkjente_ord == None:
-			alle_ord.extend(alle_nye_godkjente_ord)
-		alle_gamle_godkjente_ord = Ord.all().filter("arbeidsflytstilstand =", 1).filter("harVaertDagensOrd =", True).filter("erDagensOrd =", False).order('sisteDagensOrdDato')
-		if not alle_gamle_godkjente_ord == None:
-			alle_ord.extend(alle_gamle_godkjente_ord)
-		self.renderUsingTemplate('../../views/liste_neste.html', { 'ord':alle_ord })
-		
+    @login_required
+    def get(self):
+        alle_ord = []
+        alle_nye_godkjente_ord = Ord.all().filter("arbeidsflytstilstand =", 1).filter("harVaertDagensOrd =", False).order('forslagstidspunkt')
+        if not alle_nye_godkjente_ord == None:
+            alle_ord.extend(alle_nye_godkjente_ord)
+        alle_gamle_godkjente_ord = Ord.all().filter("arbeidsflytstilstand =", 1).filter("harVaertDagensOrd =", True).filter("erDagensOrd =", False).order('sisteDagensOrdDato')
+        if not alle_gamle_godkjente_ord == None:
+            alle_ord.extend(alle_gamle_godkjente_ord)
+        self.renderUsingTemplate('../../views/liste_neste.html', { 'ord':alle_ord })
+        
 class StemmeHandler(CoreHandler):
-	def post(self):
-		errorCode = 0
-		antallStemmer = 0
-		Authorization.authorize(self)
-		ordNokkel = long(cgi.escape(self.request.get('ord-nokkel')))
-		stemmeFor = (cgi.escape(self.request.get('erStemmeFor')) == 'true')
-		
-		ord = Ord.get_by_id(ordNokkel)
-		bidragsyterId = Bidragsyter.hent(users.get_current_user()).key().id()
-		if ord == None:
-			errorCode = 1
-		elif bidragsyterId in ord.stemmerFor or bidragsyterId in ord.stemmerMot:
-			errorCode = 2
-		else:
-			if stemmeFor:
-				ord.stemmerFor.append(bidragsyterId)
-				antallStemmer = len(ord.stemmerFor)
-			else:
-				ord.stemmerMot.append(bidragsyterId)
-				antallStemmer = len(ord.stemmerMot)
-			ord.put()
-		
-		self.renderAsJson({ 'errorCode':errorCode, 'antallStemmer':antallStemmer})
+    def post(self):
+        errorCode = 0
+        antallStemmer = 0
+        Authorization.authorize(self)
+        ordNokkel = long(cgi.escape(self.request.get('ord-nokkel')))
+        stemmeFor = (cgi.escape(self.request.get('erStemmeFor')) == 'true')
+        
+        ord = Ord.get_by_id(ordNokkel)
+        bidragsyterId = Bidragsyter.hent(users.get_current_user()).key().id()
+        if ord == None:
+            errorCode = 1
+        elif bidragsyterId in ord.stemmerFor or bidragsyterId in ord.stemmerMot:
+            errorCode = 2
+        else:
+            if stemmeFor:
+                ord.stemmerFor.append(bidragsyterId)
+                antallStemmer = len(ord.stemmerFor)
+            else:
+                ord.stemmerMot.append(bidragsyterId)
+                antallStemmer = len(ord.stemmerMot)
+            ord.put()
+        
+        self.renderAsJson({ 'errorCode':errorCode, 'antallStemmer':antallStemmer})
 
 class VisDagensOrdHandler(CoreHandler):
-	@login_required
-	def get(self):
-		dagensOrd = Ord.all().filter("erDagensOrd =", True).get()
-		self.renderUsingTemplate('../../views/dagens_ord.html', { 'ord':dagensOrd })
-		
+    @login_required
+    def get(self):
+        dagensOrd = Ord.all().filter("erDagensOrd =", True).get()
+        self.renderUsingTemplate('../../views/dagens_ord.html', { 'ord':dagensOrd })
+        
 class OrdHandler(CoreHandler):
-	def get(self, ordForesporsel):
-		if ordForesporsel == "":
-			self.redirect("/")
-		else:
-			ordStreng = urllib.unquote(ordForesporsel)
-			logging.info("Was asked for word " + ordStreng)
-			ord = Ord.all().filter("navn =", ordStreng.decode("utf-8")).get()
-			if ord == None:
-				self.error(404)
-			else:
-				self.renderUsingTemplate('../../views/dagens_ord.html', { 'ord':ord })
+    def get(self, ordForesporsel):
+        if ordForesporsel == "":
+            self.redirect("/")
+        else:
+            ordStreng = urllib.unquote(ordForesporsel)
+            logging.info("Was asked for word " + ordStreng)
+            ord = Ord.all().filter("navn =", ordStreng.decode("utf-8")).get()
+            if ord == None:
+                self.error(404)
+            else:
+                self.renderUsingTemplate('../../views/dagens_ord.html', { 'ord':ord })
--- application/model.py
+++ application/model.py
@@ -1,102 +1,101 @@
 #encoding: utf-8
-from datetime import date
-from datetime import datetime
+from datetime import date, datetime
 from google.appengine.ext import db
-import urllib, hashlib
 import base64
+import urllib
+import hashlib
 
 class Bidragsyter(db.Model):
-	googleKonto = db.UserProperty()
-	navn = db.StringProperty()
-	twitter = db.StringProperty(default="")
-	svartelistet = db.BooleanProperty(default=False)
-	bekkAdresse = db.EmailProperty()
-	paaBekkMailliste = db.BooleanProperty(default=False)
-	paaGoogleMailliste = db.BooleanProperty(default=False)
-	
-	def visningsnavn(self):
-		if self.navn == None or self.navn == "":
-			if self.googleKonto != None:
-				return self.googleKonto.nickname()
-			return "ukjent"
-		return self.navn
-	
-	@staticmethod
-	def hent(user):
-		bidragsyter = Bidragsyter.gql("WHERE googleKonto = :1", user).get()
-		if bidragsyter == None:
-			bidragsyter = Bidragsyter(navn=user.nickname(), googleKonto=user)
-			bidragsyter.put()
-		return bidragsyter
+    googleKonto = db.UserProperty()
+    navn = db.StringProperty()
+    twitter = db.StringProperty(default="")
+    svartelistet = db.BooleanProperty(default=False)
+    bekkAdresse = db.EmailProperty()
+    paaBekkMailliste = db.BooleanProperty(default=False)
+    paaGoogleMailliste = db.BooleanProperty(default=False)
+    
+    def visningsnavn(self):
+        if self.navn == None or self.navn == "":
+            if self.googleKonto != None:
+                return self.googleKonto.nickname()
+            return "ukjent"
+        return self.navn
+    
+    @staticmethod
+    def hent(user):
+        bidragsyter = Bidragsyter.gql("WHERE googleKonto = :1", user).get()
+        if bidragsyter == None:
+            bidragsyter = Bidragsyter(navn=user.nickname(), googleKonto=user)
+            bidragsyter.put()
+        return bidragsyter
 
-	def gravatarUrl(self):
-		return self.gravatarUrlWithSize(80)
-		
-	def gravatarUrlWithSize(self, size):
-		gravatar_url = "http://www.gravatar.com/avatar.php?"
-		gravatar_url += urllib.urlencode({'gravatar_id':hashlib.md5(self.googleKonto.email().lower()).hexdigest(), 'size': size })
-		return gravatar_url
-		
+    def gravatarUrl(self):
+        return self.gravatarUrlWithSize(80)
+        
+    def gravatarUrlWithSize(self, size):
+        gravatar_url = "http://www.gravatar.com/avatar.php?"
+        gravatar_url += urllib.urlencode({'gravatar_id':hashlib.md5(self.googleKonto.email().lower()).hexdigest(), 'size': size })
+        return gravatar_url
+        
 class Ord(db.Model):
-	navn = db.StringProperty()
-	beskrivelse = db.TextProperty()
-	bidragsyter = db.ReferenceProperty(Bidragsyter)
-	bidragsyter_navn = db.TextProperty(default="")
-	dagensOrdDatoer = db.ListProperty(datetime, default=[])
-	harVaertDagensOrd = db.BooleanProperty(default=False)
-	sisteDagensOrdDato = db.DateTimeProperty()
-	erDagensOrd = db.BooleanProperty(default=False)
-	stemmerFor = db.ListProperty(long, default=[])
-	stemmerMot = db.ListProperty(long, default=[])
-	arbeidsflytstilstand = db.IntegerProperty(default=0)
-	forslagstidspunkt = db.DateTimeProperty(auto_now_add=True)
-	
-	def arbeidsflytstilstand_navn(self):
-		if self.arbeidsflytstilstand == 0:
-			return "På valg"
-		if self.arbeidsflytstilstand == 1:
-			return "Godkjent"
-		return "Avslått"
-	
-	def bnavn(self):
-		if self.bidragsyter == None:
-			return self.bidragsyter_navn
-		return self.bidragsyter.visningsnavn()
+    navn = db.StringProperty()
+    beskrivelse = db.TextProperty()
+    bidragsyter = db.ReferenceProperty(Bidragsyter)
+    bidragsyter_navn = db.TextProperty(default="")
+    dagensOrdDatoer = db.ListProperty(datetime, default=[])
+    harVaertDagensOrd = db.BooleanProperty(default=False)
+    sisteDagensOrdDato = db.DateTimeProperty()
+    erDagensOrd = db.BooleanProperty(default=False)
+    stemmerFor = db.ListProperty(long, default=[])
+    stemmerMot = db.ListProperty(long, default=[])
+    arbeidsflytstilstand = db.IntegerProperty(default=0)
+    forslagstidspunkt = db.DateTimeProperty(auto_now_add=True)
+    
+    def arbeidsflytstilstand_navn(self):
+        if self.arbeidsflytstilstand == 0:
+            return "På valg"
+        if self.arbeidsflytstilstand == 1:
+            return "Godkjent"
+        return "Avslått"
+    
+    def bnavn(self):
+        if self.bidragsyter == None:
+            return self.bidragsyter_navn
+        return self.bidragsyter.visningsnavn()
 
 class Kommentar(db.Model):
-	innhold = db.StringProperty(multiline=True)
-	bidragsyter = db.ReferenceProperty(Bidragsyter)
-	kommentartidspunkt = db.DateTimeProperty(auto_now_add=True)
-	uri = db.StringProperty()
-	
-	def gravatarUrl(self):
-		return self.bidragsyter.gravatarUrlWithSize(40)
+    innhold = db.StringProperty(multiline=True)
+    bidragsyter = db.ReferenceProperty(Bidragsyter)
+    kommentartidspunkt = db.DateTimeProperty(auto_now_add=True)
+    uri = db.StringProperty()
+    
+    def gravatarUrl(self):
+        return self.bidragsyter.gravatarUrlWithSize(40)
 
 class Liker(db.Model):
-	bidragsyter = db.ReferenceProperty(Bidragsyter)
-	tidspunkt = db.DateTimeProperty(auto_now_add=True)
-	uri = db.StringProperty()
+    bidragsyter = db.ReferenceProperty(Bidragsyter)
+    tidspunkt = db.DateTimeProperty(auto_now_add=True)
+    uri = db.StringProperty()
 
-	@staticmethod
-	def antall_liker(uri):
-		return Liker.all().filter('uri = ', uri).count()
-		
-	@staticmethod
-	def fra_person(uri, bidragsyter):
-		return Liker.all().filter('uri =', uri).filter('bidragsyter =', bidragsyter).get()
-	
+    @staticmethod
+    def antall_liker(uri):
+        return Liker.all().filter('uri = ', uri).count()
+        
+    @staticmethod
+    def fra_person(uri, bidragsyter):
+        return Liker.all().filter('uri =', uri).filter('bidragsyter =', bidragsyter).get()
+    
 class Konto(db.Model):
-	navn = db.StringProperty()
-	brukernavn = db.StringProperty()
-	passord = db.StringProperty()
-	
-	@staticmethod
-	def get(navn):
-		if Konto.all().get() == None:
-			Konto(navn='dummy', brukernavn='dummy', passord = 'dummy').put()
-		return Konto.all().filter('navn = ', navn).get()
-		
-	def as_basic_auth_header(self):
-		base64string = base64.encodestring('%s:%s' % (self.brukernavn, self.passord))[:-1]
-		return "Basic %s" % base64string
-
+    navn = db.StringProperty()
+    brukernavn = db.StringProperty()
+    passord = db.StringProperty()
+    
+    @staticmethod
+    def get(navn):
+        if Konto.all().get() == None:
+            Konto(navn='dummy', brukernavn='dummy', passord = 'dummy').put()
+        return Konto.all().filter('navn = ', navn).get()
+        
+    def as_basic_auth_header(self):
+        base64string = base64.encodestring('%s:%s' % (self.brukernavn, self.passord))[:-1]
+        return "Basic %s" % base64string
