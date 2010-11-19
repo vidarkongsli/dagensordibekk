b5b5ab882220447a92794af7e2bc20eccdc3fedb
Vidar Kongsli <vidarkongsli@yahoo.com>
2010-11-19 20:14:37 +0100

    Redirect feed requests to feedburner

--- application/controllers/feed.py
+++ application/controllers/feed.py
@@ -3,6 +3,9 @@ from application.model import Ord
 
 class FeedHandler(CoreHandler):
     def get(self):
-        dagensOrd = Ord.all().filter("erDagensOrd =", True).get()
-        self.response.headers["Content-Type"] = "application/atom+xml"
-        self.renderUsingTemplate('../../views/atom_feed.xml', { 'ord':dagensOrd })
+        if self.request.headers['User-Agent'].find('FeedBurner') == -1:
+            self.redirect('http://feeds.feedburner.com/dagensordibekk', permanent=True)
+        else:
+            dagensOrd = Ord.all().filter("erDagensOrd =", True).get()
+            self.response.headers["Content-Type"] = "application/atom+xml"
+            self.renderUsingTemplate('../../views/atom_feed.xml', { 'ord':dagensOrd })
